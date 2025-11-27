98205530d135803dd270032a6c64d18886518958
Govind <govindchoudhary0981@gmail.com>
2025-11-27 17:34:42 +0530

    added part-2 notes for react

--- react-learning/src/Product.jsx
+++ react-learning/src/Product.jsx
@@ -1,6 +1,7 @@
 // Each React component is placed in its own .jsx file, and its styling is kept in a separate .css file for better structure and maintainability.
 import "./Product.css";
 
+
 // This component has its own dedicated JSX file (Product.jsx) and a matching CSS file (Product.css).
 // Product component represents a single product UI block that can be reused multiple times.
 
@@ -8,19 +9,32 @@ import "./Product.css";
 function Product({ title, price = 1, features = [] }) {
   // We receive 'features' here as an array because the parent (ProductTab) passed it as a prop.
   // Default value [] ensures map() works even if no array is provided.
-    return (
-    <div className="Product">
+
+  // Dynamic Styling with Conditions:
+  // - Here we are creating a 'styles' object that changes based on a condition.
+  // - If the price is greater than 30000, backgroundColor becomes "yellow".
+  // - Otherwise, it becomes an empty string ("") meaning no background color is applied.
+  // - This approach lets us style components dynamically using JavaScript logic.
+  let styles = { backgroundColor: price > 30000 ? "yellow" : "" };
+
+  return (
+    <div className="Product" style={styles}>
       <h3>{title}</h3>
       <h4>Price: {price}</h4>
-      <p>
         {/* Rendering array values: */}
         {/* 'features' is an array, and we use .map() to loop over each item and create <li> elements for them. */}
         {/* map() returns a NEW array of JSX elements, which React can render directly. */}
         {/* Each array element becomes one <li> item inside the UI. */}
-        {features.map((feature, idx) => (
-          <li key={idx}>{feature}</li>
-        ))}
-      </p>
+        {features.map((feature) => <li>{feature}</li>)}
+
+        {/* Conditional Rendering:
+            - In React, we can conditionally show UI using ternary operators.
+            - Here, we check if 'price' is greater than 30000.
+            - If TRUE, React renders the <p>Discount of 5%</p>.
+            - If FALSE, React renders 'null', which means render nothing.
+        */}
+        {price > 30000 ? <p>Discount of 5%</p> : null}
+      
     </div>
   );
 }
