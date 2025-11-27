7b8eafd717b7a75046219940b752aca6bd2b296a
Govind <govindchoudhary0981@gmail.com>
2025-11-27 20:40:19 +0530

    added new part-3 notes

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -2,6 +2,8 @@
 import "./App.css";
 import Title from "./Title.jsx";
 import ProductTab from "./ProductTab.jsx";
+import Button from "./Button.jsx";
+import Form from "./Form.jsx";
 
 // Another functional component named Description.
 // Each component can return only ONE parent element.
@@ -15,7 +17,9 @@ function Description() {
 function App() {
   let name = "casey";
   return (
-    <> {/* Using React Fragment (<></>) to wrap multiple elements without adding extra HTML tags to the DOM */}
+    <>
+      {" "}
+      {/* Using React Fragment (<></>) to wrap multiple elements without adding extra HTML tags to the DOM */}
       {/* Rendering JSX elements inside the main App component */}
       <h1>This is my app component</h1>
       <p>Inside app component we have: </p>
@@ -26,7 +30,17 @@ function App() {
       <h3>Hi, {name.toUpperCase()}</h3>
       <hr />
       {/* Rendering ProductTab component where repeated Product components are grouped instead of directly writing them here. */}
-      <ProductTab/>
+      <ProductTab />
+      {/* Rendering the Button component. 
+          Clicking the button triggers a React event (onClick) 
+          which calls the handleOnClick() function defined inside Button.jsx.
+      */}
+      <Button />
+      {/* Rendering the Form component.
+          When the form is submitted, React passes an event object to handleFormSubmit,
+          and preventDefault() stops the page from refreshing.
+      */}
+      <Form />
     </>
   );
 }
